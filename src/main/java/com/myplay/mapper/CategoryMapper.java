f6dcc176c3291ee4489aedfe058845c97260929c
wangxiao-wnn <1299989003@qq.com>
2019-08-19 20:41:26 +0800

    @Mapper

--- src/main/java/com/myplay/mapper/CategoryMapper.java
+++ src/main/java/com/myplay/mapper/CategoryMapper.java
@@ -3,6 +3,8 @@ package com.myplay.mapper;
 import com.myplay.model.Category;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
 public interface CategoryMapper {
     int deleteByPrimaryKey(Integer id);
 
